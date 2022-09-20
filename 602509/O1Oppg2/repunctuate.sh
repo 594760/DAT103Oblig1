592fcebeacd04e26e7cbf3ced6bb3d99ccc43be2
h602509 <hansdevik@gmail.com>
2022-09-20 11:59:38 +0200

    Nesten ferdig task2

--- 602509/O1Oppg2/repunctuate.sh
+++ 602509/O1Oppg2/repunctuate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-dep=/home/hans/OneDrive/Skole/HVL/DAT103/Oblig1/602509/O1Oppg2/task2.txt
-cd $1
+#dep=/home/hans/OneDrive/Skole/HVL/DAT103/Oblig1/602509/O1Oppg2/task2.txt
+
 rm -f task3.txt
 touch task3.txt
-cat $dep > task3.txt
+cat task2.txt > task3.txt
+
+cd $1
 # leser linje for linje og hasjer linjer uten bokstaver || tall
 
     
@@ -17,9 +19,28 @@ while IFS="" read -r p || [ -n "$p" ]; do
         hs=${str::-4}
 
         if [ "$p" == "$hs" ]; then
-            sed -i "s/$p/$(cat $f)/" task3.txt
+            sed -i "s/$p/$(cat $f)/" ../task3.txt
         fi
 
         
     done
-done < task3.txt
+done < ../task3.txt
+cd ..
+
+#tr -s '\n' < task3.txt
+cat task3.txt | tr '\n' ' ' > test.txt
+
+echo "------------------- test.txt -------------------"
+cat test.txt
+
+#TODO her må vi få vekk mellomrommet etter alle ^alnum
+
+
+
+
+cat test.txt | sed 's/|/\n/g' > task3.txt
+
+echo $"\n\n"
+echo "-------------------- task3.txt --------------------"
+
+cat task3.txt
