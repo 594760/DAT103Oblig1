a18ebf24d130a6705efb5668680babf99d793856
594760 <594760@stud.hvl.no>
2022-09-21 10:36:03 +0200

    Fikset

--- oblig1-group10/repunctuate.sh
+++ oblig1-group10/repunctuate.sh
@@ -3,15 +3,13 @@
 #dep=/home/hans/OneDrive/Skole/HVL/DAT103/Oblig1/602509/O1Oppg2/task2.txt
 
 rm -f task3.txt
+rm -f test.txt
 touch task3.txt
-cat task2.txt > task3.txt
+touch test.txt
+cat task2.txt > test.txt
 
 cd $1
 # leser linje for linje og hasjer linjer uten bokstaver || tall
-
-    
-
-
 while IFS="" read -r p || [ -n "$p" ]; do
     for f in ./*
     do
@@ -19,28 +17,14 @@ while IFS="" read -r p || [ -n "$p" ]; do
         hs=${str::-4}
 
         if [ "$p" == "$hs" ]; then
-            sed -i "s/$p/$(cat $f)/" ../task3.txt
+            sed -i "s/$p/$(cat $f)/" ../test.txt
         fi
 
         
     done
-done < ../task3.txt
+done < ../test.txt
 cd ..
 
-#tr -s '\n' < task3.txt
-cat task3.txt | tr -d '\n' > test.txt
+tr -d '\n' <test.txt | sed 's/|/\n/g' > task3.txt
 
-echo "------------------- test.txt -------------------"
-cat test.txt
-
-#TODO her må vi få vekk mellomrommet etter alle ^alnum
-
-
-
-
-cat test.txt | sed 's/|/\n/g' > task3.txt
-
-echo $"\n\n"
-echo "-------------------- task3.txt --------------------"
-
-cat task3.txt
+rm -f test.txt
