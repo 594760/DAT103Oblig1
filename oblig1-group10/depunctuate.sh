e12565f8246220e061b8054679fdbc0324c44303
594760 <594760@stud.hvl.no>
2022-09-26 16:45:12 +0200

    rettet etter jusuts tilbakemelding

--- oblig1-group10/depunctuate.sh
+++ oblig1-group10/depunctuate.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 # lager nye filer
-touch t1.temp
-touch t2.temp
+touch "t1.temp"
+touch "t2.temp"
 
 # setter variabler som peker til filene
 t1="t1.temp"
 t2="t2.temp"
-ut=task2.txt
 
 # lage hash-mappe
-mkdir -p $1
+mkdir -p "$1"
 
 # kopierer inn standard input
-cat > $t1
+cat > "$t1"
 
 # fjerner linjeshift og substituerer med "|"
 
-tr '\n' '|' <$t1 | sed -E 's/[^[:alnum:]]+/\n&\n/g' >$t2
+tr '\n' '|' <"$t1" | sed -E 's/[^[:alnum:]]+/\n&\n/g' >"$t2"
 
 # leser linje for linje og hasjer linjer uten bokstaver || tall
 IFS=""
-cat $t2 |
-    while read data; do
+cat "$t2" |
+    while read "data"; do
 
         # sorterer ut linjer ute bokstaver og tall
-        if [[ $data =~ [^[:alpha:]] ]]; then
+        if [[ "$data" =~ [^[:alpha:]] ]]; then
 
             # finner ut om filen eksister fra før av
             if [[ -f "./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g')" ]]; then
@@ -33,7 +32,7 @@ cat $t2 |
                 pathToFile="./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g')"
 
                 # kjører vis det er feil med hash filen
-                if [[ $data != $(cat $pathToFile) ]]; then
+                if [[ "$data" != $(cat $pathToFile) ]]; then
                     echo "filname does not match hash of content. "
                     echo "Path to file: $pathToFile"
                     exit 1
@@ -41,14 +40,14 @@ cat $t2 |
 
             else
 
-                echo -n $data >./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g')
+                echo -n "$data" >"$1"/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g')
 
             fi
 
-            echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g'
+            echo -n "$data" | sha256sum | sed -E 's/[^[:alnum:]]+//g'
 
         else
-            echo $data
+            echo "$data"
         fi
 
     done
