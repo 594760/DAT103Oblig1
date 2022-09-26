1df788ad1b6823a71705902825fc420f77442657
594760 <594760@stud.hvl.no>
2022-09-26 13:51:02 +0200

    Endringer i depunctuate.sh

--- oblig1-group10/depunctuate.sh
+++ oblig1-group10/depunctuate.sh
@@ -15,26 +15,13 @@ ut=task2.txt
 
 # lage hash-mappe
 mkdir -p $1
+echo $1
 
 # kopierer inn standard input
-echo "Read from following file:" 
-read input
-
-# denne funker på sanity checken men har ikke fått den til i VS Code terminal
-#cat > $t1
-
-if [[ -f "$input" ]]; then
-    cat $input >$t1
-else
-    # sletter
-    rm t1.temp
-    rm t2.temp
-    rm $ut
-    echo "depunctuate: File dose not exsist, exiting the script."
-    exit 1
-fi
+cat > $t1
 
 # fjerner linjeshift og substituerer med "|"
+
 tr '\n' '|' <$t1 | sed -E 's/[^[:alnum:]]+/\n&\n/g' >$t2
 
 # leser linje for linje og hasjer linjer uten bokstaver || tall
@@ -46,9 +33,9 @@ cat $t2 |
         if [[ $data =~ [^[:alpha:]] ]]; then
 
             # finner ut om filen eksister fra før av
-            if [[ -f "./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g').txt" ]]; then
+            if [[ -f "./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g')" ]]; then
 
-                pathToFile="./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g').txt"
+                pathToFile="./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g')"
 
                 # kjører vis det er feil med hash filen
                 if [[ $data != $(cat $pathToFile) ]]; then
@@ -59,14 +46,14 @@ cat $t2 |
 
             else
 
-                echo -n $data >./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g').txt
+                echo -n $data >./$1/$(echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g')
 
             fi
 
-            echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g' >>$ut
+            echo -n $data | sha256sum | sed -E 's/[^[:alnum:]]+//g'
 
         else
-            echo $data >>$ut
+            echo $data
         fi
 
     done
@@ -74,6 +61,3 @@ cat $t2 |
 # sletter
 rm t1.temp
 rm t2.temp
-
-# usikker på om vi må ha denne
-exit 0
